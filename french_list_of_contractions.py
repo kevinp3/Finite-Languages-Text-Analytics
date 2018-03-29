e6859ce1ccd8f6f34e40cfb3def765e64f949529
finitelanguages <37458054+finitelanguages@users.noreply.github.com>
2018-03-29 07:48:31 -0700

    french contraction list added

---
+++ french_list_of_contractions.py
@@ -0,0 +1,38 @@
+#List of contractions and corresponding expansions
+#exceptions that are not contracted are listed first so they replace with themselves
+
+french_contractions = {
+"aujourd'hui": "aujourd'hui"
+"d'abord": "d'abord"
+"d'accord": "d'accord"
+"d'ac": "d'ac"
+"d'ailleurs": "d'ailleurs"
+"d'après": "d'après"
+"d'habitude": "d'habitude"
+"presqu'île": "presqu'île"
+"quelqu'un": "quelqu'un"
+"s'il": "s'il"
+"s'ils": "s'ils"
+"au": "à le",
+"aux": "à les",
+"du": "de le",
+"des": "de les",
+"auquel": "à lequel",
+"auxquels": "à lesquels",
+"auxquelles": "à lesquelles",
+"duquel": "de lequel",
+"desquels": "de lesquels",
+"desquelles": "de lesquelles",
+"voici": "vois ci",
+"voilà": "vois là",
+"ès": "en les",
+"l'": "la",
+"c'est": "ce est",
+"d'": "de",
+"j'": "je",
+"j'y": "je y",
+"m'": "me",
+"n'est": "ne est",
+"'puisqu'on": "puisque on",
+"lorsqu'il": "lorsque il",
+}
